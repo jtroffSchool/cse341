894f441dbbd7c35cebb27c6df2c10aed71b8195d
jtroffSchool <jtroff77@gmail.com>
2023-05-10 16:46:13 -0600

    Added passport github

--- personalProject/server.js
+++ personalProject/server.js
@@ -3,6 +3,7 @@ const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
@@ -14,10 +15,32 @@ const port = process.env.PORT || 3000;
 app
 	.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 	.use(bodyParser.json())
+	.use(
+		session({
+			secret: 'secret',
+			resave: false,
+			saveUninitialized: true,
+		})
+	)
+	//This is the basic express session({..}) initialization.
+	.use(passport.initialize())
+	// init passport on every route call
+	.use(passport.session())
+	//allow passport to use express-session
 	.use((req, res, next) => {
 		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader(
+			'Access-Control-Allow-Header',
+			'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
+		);
+		res.setHeader(
+			'Access-Control-Allow-Methods',
+			'POST, GET, PUT, PATCH, OPTIONS, DELETE'
+		);
 		next();
 	})
+	.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+	.use(cors({ origin: '*' }))
 	.use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
